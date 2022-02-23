ad4e04175ac6fd2aa89eeb9b475f8c7d9fe2d1ac
shawrylk <33708806+shawrylk@users.noreply.github.com>
2022-02-23 10:40:08 +0700

    done first step in learning identity core
    test project

---
+++ Pages/Identity/Admin/_AdminWorkflows.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Identity.Admin
+{
+    public class _AdminWorkflowsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
